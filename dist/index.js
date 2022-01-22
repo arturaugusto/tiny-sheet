a6282a76d29aa0186586addb5b236c232d4c4c5b
Artur Augusto Martins <arturaugusto@gmail.com>
2022-01-22 10:50:54 -0300

    fix export

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.tinySheet = tinySheet;
-
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
 function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
